3b52701d055cc023444f6fec78809f9e2b66b2e9
Dallen Hoyal <dallen.hoyal@gmail.com>
2020-02-04 23:50:05 -0700

    pretty much done

--- Project_4_Coffee_Machine/coffee.py
+++ Project_4_Coffee_Machine/coffee.py
@@ -1,14 +1,57 @@
+VALID_AMOUNTS = [5, 10, 25, 50]
+
 
 class CoffeeMachine():
     def __init__(self):
         self._cashBox   = CashBox()
-        self._selector  = Selector()
+        self._selector  = Selector(self._cashBox)
         
-    def oneAction():
-        print("TODO: oneAction")
+    def oneAction(self):
+        command = input(">>> Your command: ")
+        command = command.split(" ")
         
-    def totalCash():
-        print("TODO: totalCash")
+        if command[0] == "select":
+            selection = None
+            if not len(command) > 1:
+                print("INPUT ERROR >>>")
+                print("You didn't select anything.")
+            else:
+                selection = int(command[1])
+                
+                if selection in range(1, 6):
+                    self._selector.select(selection)
+                else:
+                    print("INPUT ERROR >>>")
+                    print(f"{selection} is not a product.")
+            
+        elif command[0] == "insert":
+            amount = None
+            if not len(command) > 1:
+                print("INPUT ERROR >>>")
+                print("You didn't insert anything.")
+            else:
+                amount = int(command[1])
+              
+                if amount in VALID_AMOUNTS:
+                    self._cashBox.deposit(amount)
+                else:
+                    print("INPUT ERROR >>>")
+                    print("We only take half-dollars, quarters, dimes, and nickels.")
+                    print("Coin(s) returned")            
+            
+        elif command[0] == "cancel":
+            self._cashBox.returnCoins()
+            
+        elif command[0] == "quit":
+            return False
+            
+        else:
+            print("Invalid command")
+            
+        return True
+        
+    def totalCash(self):
+        return self._cashBox.total()
         
         
 class CashBox():
@@ -17,46 +60,68 @@ class CashBox():
         self._totalReceived = 0
         
     def deposit(self, amount):
-        print('TODO: deposit')
+        self._credit += amount
+        self._totalReceived += amount
+        print(f"Depositing ${amount/100:.2f}.  You have ${self._credit/100:.2f} credit.")
         
     def returnCoins(self):
-        print("TODO: returnCoins")
+        print(f"Returning ${self._credit/100:.2f}.")
     
     def haveYou(self, amount):
-        print("TODO: haveYou, returns type bool")
+        return amount <= self._credit
         
     def deduct(self, amount):
-        print("TODO: deduct")
+        self._totalReceived += amount
+        self._credit -+ amount
+        self.returnCoins()
         
     def total(self):
-        print("TODO: total, returns type int")
+        return self._totalReceived
         
 
 class Selector():
-    def __init__(self):
-        self._cashBox   = CashBox()
-        self._products  = []
+    def __init__(self, cashBox):
+        self._cashBox   = cashBox
+        self._products  = {
+            1: Product("black", 35, ["cup", "coffee", "water"]),
+            2: Product("white", 35, ["cup", "coffee", "creamer", "water"]),
+            3: Product("sweet", 35, ["cup", "coffee", "sugar", "water"]),
+            4: Product("white & sweet", 35, ["cup", "coffee", "sugar", "creamer", "water"]),
+            5: Product("bouillon", 25, ["cup", "bouillonPowder", "water"])
+        }
         
     def select(self, choiceIndex):
-        print("TODO: select")
-
+        product = self._products[choiceIndex]
+        price = product.getPrice()
+        have = self._cashBox.haveYou(price)
+        
+        if have:
+            product.make()
+            self._cashBox.deduct(price)
+        else:
+            print("Sorry. Not enough money deposited.")
 
 
 class Product():
-    def __init__(self):
-        self._name      = ""
-        self._price     = 0,
-        self._recipe    = ["", "", ""]
+    def __init__(self, name, price, recipe):
+        self._name      = name
+        self._price     = price,
+        self._recipe    = recipe
         
-    def getPrice():
-        print("TODO: getPrice, returns type int")
+    def getPrice(self):
+        return self._price[0]
         
-    def make():
-        print("TODO: make")
+    def make(self):
+        for item in self._recipe:
+            print(f"\tDispensing {item}")
 
 
 def main():
-    
+    m = CoffeeMachine()
+    while m.oneAction():
+        pass
+    total = m.totalCash()
+    print(f"Total Cash: ${total/100:.2f}")
     
     
 if __name__ == "__main__":
